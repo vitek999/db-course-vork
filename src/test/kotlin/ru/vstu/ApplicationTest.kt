4083aa258cda6f14e199419011b9e6ae74d4665e
Viktor.Noskin <viktor.noskin@jetbrains.com>
2022-01-08 17:53:42 +0300

    refactoring: clean unused imports in test sources

--- src/test/kotlin/ru/vstu/ApplicationTest.kt
+++ src/test/kotlin/ru/vstu/ApplicationTest.kt
@@ -1,16 +1,10 @@
 package ru.vstu
 
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.locations.*
-import io.ktor.features.*
-import io.ktor.serialization.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import ru.vstu.plugins.*
+import ru.vstu.plugins.configureRouting
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
