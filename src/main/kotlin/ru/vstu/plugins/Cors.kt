beb0fedf49309d80f70e702cbe0224e385af7030
Viktor.Noskin <viktor.noskin@jetbrains.com>
2022-01-16 23:30:38 +0300

    fix: cors for react

--- src/main/kotlin/ru/vstu/plugins/Cors.kt
+++ src/main/kotlin/ru/vstu/plugins/Cors.kt
@@ -6,9 +6,16 @@ import io.ktor.http.*
 
 fun Application.configureCORS() {
     install(CORS) {
-        method(HttpMethod.Get)
-        method(HttpMethod.Post)
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
         method(HttpMethod.Delete)
-        anyHost()
+        method(HttpMethod.Patch)
+        method(HttpMethod.Post)
+        header(HttpHeaders.Authorization)
+        header(HttpHeaders.AccessControlAllowHeaders)
+        header(HttpHeaders.ContentType)
+        header(HttpHeaders.AccessControlAllowOrigin)
+        header("MyCustomHeader")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
 }
